a8b079f5bf08f45a3772c6bb6533daf98843a938
virgantara <vinux.edu@gmail.com>
2022-09-29 10:33:29 +0700

    + print per frame

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import ouster.sdk.viz
+import os
 from ouster.sdk import viz
 from ouster import client
 from ouster import pcap
@@ -14,91 +15,58 @@ hostname = 'os-122215001365.local'
 lidar_port = 7502
 imu_port = 7503
 
+from itertools import islice
+
+csv_dir = "csv"
+csv_base = "test"
+csv_ext = "csv"
+meta_path = "OS-1-32-U2_122215001365_1024x10_20220929_100745.json"
+pcap_path = "OS-1-32-U2_122215001365_1024x10_20220929_100745.pcap"
+# precompute xyzlut to save computation in a loop
+with open(meta_path, 'r') as f:
+    info = client.SensorInfo(f.read())
+
+source = pcap.Pcap(pcap_path, info)
+metadata = source.metadata
+print(info)
+xyzlut = client.XYZLut(metadata)
+# create an iterator of LidarScans from pcap and bound it if num is specified
+
+scans = iter(client.Scans(source))
+# if num:
+#     scans = islice(scans, num)
+counter=0
+for idx, scan in enumerate(scans):
+    # initialize the field names for csv header
+    counter += 1
+    # if not field_names or not field_fmts:
+    #     field_names, field_fmts = get_fields_info(scan)
+
+    # copy per-column timestamps for each channel
+    timestamps = np.tile(scan.timestamp, (scan.h, 1))
+    # grab channel data
+    fields_values = [scan.field(ch) for ch in scan.fields]
+
+    # use integer mm to avoid loss of precision casting timestamps
+    xyz = (xyzlut(scan.field(client.ChanField.RANGE)) * 1000).astype(np.int64)
 
 
-# ranges = scan.field(client.ChanField.RANGE)
-# destagger ranges, notice `metadata` use, that is needed to get
-# sensor intrinsics and correctly data transforms
-# ranges_destaggered = client.destagger(source.metadata, ranges)
-# ranges_destaggered = (ranges_destaggered / np.max(ranges_destaggered) * 255).astype(np.uint8)
-# ranges_destaggered = cv2.resize(ranges_destaggered,(1600, 200))
-# plt.imshow(ranges_destaggered, cmap='gray', resample=False)
-# plt.show()
-# scan = client.LidarScan(
-#     info.format.pixels_per_column,
-#     info.format.columns_per_frame,
-#     info.format.udp_profile_lidar
-# )
-#
-#
-# signal = scan.field(client.ChanField.RANGE)
-# signal = client.destagger(meta, signal)
-# print(signal.shape)
+    # get all data as one H x W x num fields int64 array for savetxt()
+    frame = np.dstack((timestamps, *fields_values, xyz))
 
 
-# signal = np.divide(signal, np.amax(signal), dtype=np.float32)
-# startLidar(hostname,lidar_port,imu_port)
-# stream_range_and_reflectivity()
-# Creating a point viz instance
+    # not necessary, but output points in "image" vs. staggered order
+    frame = client.destagger(metadata, frame)
+    print(xyz.shape)
+    print(frame.shape)
+    # write csv out to file
 
-# point_viz = viz.PointViz("Example Viz")
-# viz.add_default_controls(point_viz)
-# # ... add objects here
-# update internal objects buffers and run visualizer
-#
-# try:
-#
-#     with closing(client.Scans.stream(hostname, lidar_port, complete=False)) as stream:
-#         show = True
-#         # while show:
-#         for scan in stream:
-#             meta = stream.metadata
-#             signal = scan.field(client.ChanField.REFLECTIVITY)
-#             signal = client.destagger(meta, signal)
-#             signal = np.divide(signal, np.amax(signal), dtype=np.float32)
-#             xyzlut = client.XYZLut(meta)
-#             xyz = xyzlut(scan.field(client.ChanField.RANGE))
-#             cloud_xyz = viz.Cloud(xyz.shape[0] * xyz.shape[1])
-#             cloud_xyz.set_xyz(np.reshape(xyz, (-1, 3)))
-#             cloud_xyz.set_key(signal.ravel())
-#             point_viz.add(cloud_xyz)
-#             point_viz.update()
-#             point_viz.run()
-#             # print(meta)
-#             # img_aspect = (meta.beam_altitude_angles[0] - meta.beam_altitude_angles[-1]) / 360.0
-#             # img_screen_height = 0.4  # [0..2]
-#             # img_screen_len = img_screen_height / img_aspect
-#             #
-#             # # signal adalah citra proyeksi 360 derajat
-#             # # signal = client.destagger(stream.metadata, scan.field(client.ChanField.REFLECTIVITY))
-#             # # signal = (signal / np.max(signal) * 255).astype(np.uint8)
-#             # # signal = cv2.resize(signal, (1800, 250))
-#             #
-#             # # range adalah citra 2.5-dimensi
-#             # range = client.destagger(stream.metadata, scan.field(client.ChanField.RANGE))
-#             # range = (range / np.max(range) * 255).astype(np.uint8)
-#             # range = cv2.resize(range, (1800, 250))
-#             # range_img = viz.Image()
-#             # range_img.set_image(range)
-#             # range_img.set_position(-img_screen_len / 2, img_screen_len / 2, 1 - img_screen_height, 1)
-#             # point_viz.add(range_img)
-#
-#                 #
-#                 # img_vstack = np.vstack((signal, range))
-#                 # cv2.imshow("Range & Reflectivity (Press Q to Close)", img_vstack)
-#                 # cv2.imshow("scaled reflectivity", range)
-#                 # key = cv2.waitKey(1) & 0xFF
-#                 # if key & 0xFF == ord('q'):
-#                 #     show = False
-#                 #     break
-#     # cv2.destroyAllWindows()
-# except Exception as e:
-#     print("Error:", e)
-
-
-
-# stopLidar(hostname)
-
+    # csv_path = os.path.join(csv_dir, f'{csv_base}_{idx:06d}.{csv_ext}')
+    # print(f'write frame #{idx}, to file: {csv_path}')
+    # header = '\n'.join([f'frame num: {idx}', field_names])
+    #
+    # np.savetxt(csv_path, frame.reshape(-1, frame.shape[2]), fmt=field_fmts,delimiter=',',header=header)
+# print("Total",counter)
 # startLidar(hostname, lidar_port, imu_port)
 # streamLidar(hostname, lidar_port)
 # stopLidar(hostname)
